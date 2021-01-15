7cbffe1877e1f204be1a0aa24fd6f30dfd33dac1
Saldi Sucipto <saldisucipto@gmail.com>
2021-01-15 21:56:09 +0700

    Spliting Theme Template Frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,20 @@
-@extends('layouts.backend.master')
+@extends('layouts.frontend.master')
 
 @section('content')
+{{-- Startd Header --}}
+@include('layouts.frontend.header')
+{{-- End Header --}}
+
+{{-- Strat Slider --}}
+@include('layouts.frontend.slider')
+{{-- End Slider --}}
+
+{{-- Produk Section --}}
+@include('layouts.frontend.produk')
+{{-- End Produk Section --}}
+
+{{-- Footer Start --}}
+@include('layouts.frontend.footer')
+{{-- End Footer --}}
 
 @endsection
