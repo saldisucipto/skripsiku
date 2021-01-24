76b9c0f39914ff100f20fc35e6d3bf77c9a5000f
Saldi Sucipto <saldisucipto@gmail.com>
2021-01-24 21:55:48 +0700

    Dropdown Belum Sempurna

--- app/Http/Controllers/StaticController.php
+++ app/Http/Controllers/StaticController.php
@@ -3,18 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\CompanyInfo;
 use App\BannerSlider;
+use App\Navigasi;
+use App\ParentNavigasi;
 
 class StaticController extends Controller
 {
     // index welcome
     function welcome(){
+        $routeName = Route::getCurrentRoute()->uri();
         $companyInfo = CompanyInfo::get()->first();
         $slider = BannerSlider::get()->all();
+        $navigasi = Navigasi::with('parent')->get();
+        $parentNav = ParentNavigasi::with('navigasi')->get();
         return view('welcome', [
+            'routeName' => $routeName,
             'companyInfo' => $companyInfo,
-            'slider' => $slider
+            'slider' => $slider,
+            'parentNav' => $parentNav,
+            'navigasi' => $navigasi
         ]);
     }
 }
--- resources/views/layouts/frontend/header.blade.php
+++ resources/views/layouts/frontend/header.blade.php
@@ -1,43 +1,49 @@
-    <div class="header-middle"><!--header-middle-->
+    <div class="header-middle">
+        <!--header-middle-->
         <div class="container">
             <div class="row">
                 <div class="col-sm-4">
                     <div class="logo pull-left">
-                        <a href="/"><img src="{{asset('logo.png')}}" alt="PT. Cipta Aneka Air" /></a>
+                        <a href="/"><img src="{{ asset('logo.png') }}" alt="PT. Cipta Aneka Air" /></a>
                     </div>
                 </div>
                 <div class="col-sm-8">
                     <div class="shop-menu pull-right">
                         <ul class="nav navbar-nav">
                             <li><a href="/register"><i class="fa fa-user"></i> Register </a></li>
-                            {{-- <li><a href="#"><i class="fa fa-star"></i> Wishlist</a></li>
+                            {{-- <li><a href="#"><i class="fa fa-star"></i> Wishlist</a>
+                            </li>
                             <li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li>
-                            <li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Cart</a></li> --}}
+                            <li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Cart</a></li>
+                            --}}
                             <li><a href="/login"><i class="fa fa-lock"></i> Login</a></li>
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
-    </div><!--/header-middle-->
+    </div>
+    <!--/header-middle-->
 
     <div class="corak-air">
         <div class="container">
             <div class="row">
                 <div class="background-corak">
-                    <img src="{{asset('corak.png')}}" alt="">
+                    <img src="{{ asset('corak.png') }}" alt="">
                 </div>
             </div>
         </div>
     </div>
 
 
-    <div class="header-bottom"><!--header-bottom-->
+    <div class="header-bottom">
+        <!--header-bottom-->
         <div class="container">
             <div class="row">
                 <div class="col-sm-9">
                     <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse"
+                            data-target=".navbar-collapse">
                             <span class="sr-only">Toggle navigation</span>
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
@@ -46,33 +52,22 @@
                     </div>
                     <div class="mainmenu pull-left">
                         <ul class="nav navbar-nav collapse navbar-collapse">
-                            <li><a href="index.html" class="active">Home</a></li>
-                            <li class="dropdown"><a href="#">Shop<i class="fa fa-angle-down"></i></a>
-                                <ul role="menu" class="sub-menu">
-                                    <li><a href="shop.html">Products</a></li>
-                                    <li><a href="product-details.html">Product Details</a></li>
-                                    <li><a href="checkout.html">Checkout</a></li>
-                                    <li><a href="cart.html">Cart</a></li>
-                                    <li><a href="login.html">Login</a></li>
-                                </ul>
-                            </li>
-                            <li class="dropdown"><a href="#">Blog<i class="fa fa-angle-down"></i></a>
-                                <ul role="menu" class="sub-menu">
-                                    <li><a href="blog.html">Blog List</a></li>
-                                    <li><a href="blog-single.html">Blog Single</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="404.html">404</a></li>
-                            <li><a href="contact-us.html">Contact</a></li>
+                            <li><a href="/" class=" @if ($routeName==='/' ) active @endif">Home</a></li>
+                            @foreach ($parentNav as $item)
+                                <li><a href="{{ $item->link }}" class="    @if ($routeName==$item->link) active @endif">{{ $item->title }}</a></li>
+                            @endforeach
+
                         </ul>
                     </div>
                 </div>
                 <div class="col-sm-3">
                     <div class="search_box pull-right">
-                        <input type="text" placeholder="Search"/>
+                        <input type="text" placeholder="Search" />
                     </div>
                 </div>
             </div>
         </div>
-    </div><!--/header-bottom-->
-</header><!--/header-->
+    </div>
+    <!--/header-bottom-->
+    </header>
+    <!--/header-->
